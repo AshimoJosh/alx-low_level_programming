e6382c658a1b85f4d4b94dfbb07f704900662321
AshimoJosh <ashimojoshuahassan@gmail.com>
2023-10-22 18:30:46 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
- *
- * Return: Always 0.
- */
+* main - program that prints the numbers from 1 and 50
+* Return: Always 0.
+*/
 
 int main(void)
 {
 	char letter;
 
-	for (letter = '0'; letter <= '9'; letter++)
+	for (letter = 0; letter <= 9; letter++)
 
 	{
 	putchar(letter);
